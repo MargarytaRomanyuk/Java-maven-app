7c7eb3dab58ad6ff2870ebea0e23951131fed0c4
Marharita Romaniuk <puma28purple@gmail.com>
2023-02-07 00:20:29 +0300

    Update groovi

--- script.groovy
+++ script.groovy
@@ -1,24 +1,20 @@
 def buildJar() {
-    echo "building the application..."
+    echo "Testing and building the application..."
+    sh 'mvn test'
     sh 'mvn package'
-    sh 'mvn --version'
-    sh 'ls -la'
 } 
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "Building the docker image..."
    withCredentials([usernamePassword(credentialsId: 'dockerhub-credenntials', passwordVariable: 'PASSWD', usernameVariable: 'USER')]) {
        sh 'docker build -t magharyta/my-repo:jma-1.1.0 .'
        sh "echo $PASSWD | docker login -u $USER --password-stdin"
        sh 'docker push magharyta/my-repo:jma-1.1.0'
-       sh 'ls -la'
-       sh 'pwd'
-       sh 'docker --version'
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo 'Deploying the application...'
 } 
 
 return this
