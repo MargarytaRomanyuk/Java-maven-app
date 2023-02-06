4a4360440bfae3c3c72e178a4b8167cd95c93864
MargarytaRomanyuk <106387414+MargarytaRomanyuk@users.noreply.github.com>
2023-02-06 21:03:50 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,18 @@
 def buildJar() {
     echo "building the application..."
-    // sh 'mvn package'
+    sh 'mvn package'
     sh 'mvn --version'
 } 
 
 def buildImage() {
     echo "building the docker image..."
    withCredentials([usernamePassword(credentialsId: 'dockerhub-credenntials', passwordVariable: 'PASSWD', usernameVariable: 'USER')]) {
-       // sh 'docker build -t magharyta/my-repo:jma-1.1.0 .'
-       // sh "echo $PASSWD | docker login -u $USER --password-stdin"
-       // sh 'docker push  push magharyta/my-repo:jma-1.1.0'
+       sh 'docker build -t magharyta/my-repo:jma-1.1.0 .'
+       sh "echo $PASSWD | docker login -u $USER --password-stdin"
+       sh 'docker push  push magharyta/my-repo:jma-1.1.0'
        sh 'ls -la'
        sh 'pwd'
        sh 'docker --version'
-       sh "echo $USER"
     }
 } 
 
