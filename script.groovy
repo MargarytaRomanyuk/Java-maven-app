636226db285e314f72b0ef498cb635cb657b17f7
MargarytaRomanyuk <106387414+MargarytaRomanyuk@users.noreply.github.com>
2023-02-07 21:00:57 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -11,9 +11,9 @@ def buildJar() {
 def buildImage() {
     echo "Building the docker image..."
    withCredentials([usernamePassword(credentialsId: 'dockerhub-credenntials', passwordVariable: 'PASSWD', usernameVariable: 'USER')]) {
-       sh 'docker build -t magharyta/my-repo:jma-1.1.0 .'
+       sh 'docker build -t magharyta/my-repo:$IMAGE_NAME .'
        sh "echo $PASSWD | docker login -u $USER --password-stdin"
-       sh 'docker push magharyta/my-repo:jma-1.1.0'
+       sh 'docker push magharyta/my-repo:$IMAGE_NAME'
     }
 } 
 
